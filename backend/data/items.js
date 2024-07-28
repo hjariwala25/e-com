fcb1aac92a8897e1c6309b08a2da689018f22722
hjariwala25 <hjariwala150@gmail.com>
2024-07-28 21:12:48 +0530

    fix

--- backend/data/items.js
+++ backend/data/items.js
@@ -2,7 +2,7 @@ const fs = require('node:fs/promises');
 const path = require("path")
 
 async function getStoredItems() {
-  const resolvedPath = path.resolve("public", "items.json")
+  const resolvedPath = path.resolve("/public/items.json")
   const rawFileContent = await fs.readFile(resolvedPath, { encoding: 'utf-8' });
   const data = JSON.parse(rawFileContent);
   const storedItems = data.items ?? [];
