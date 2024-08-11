24f409d88ec1f806b5b739f399ef524cb396464c
hjariwala25 <hjariwala150@gmail.com>
2024-08-11 21:17:44 +0530

    update

--- frontend/src/components/FetchItems.jsx
+++ frontend/src/components/FetchItems.jsx
@@ -4,32 +4,30 @@ import { itemsActions } from "../store/itemSlice";
 import { fetchStatusActions } from "../store/fetchStatusSlice";
 
 const FetchItems = () => {
-const fetchStatus = useSelector((store) => store.fetchStatus);
-const dispatch = useDispatch();
+  const fetchStatus = useSelector((store) => store.fetchStatus);
+  const dispatch = useDispatch();
 
-useEffect(() => {
+  console.log(`${import.meta.env.VITE_SOME_KEY}/items`);
+
+  useEffect(() => {
     if (fetchStatus.fetchDone) return;
-    const controller = new AbortController();
-    const signal = controller.signal;
+    // const controller = new AbortController();
+    // const signal = controller.signal;
 
     // Fetch Data
     dispatch(fetchStatusActions.markFetchingStarted());
-    fetch(`${import.meta.env.API_ENDPOINT}/items`, {signal})
-    .then((res) => res.json())
-    .then(({ items }) => {
+
+    fetch(`${import.meta.env.VITE_SOME_KEY}/items`,{
+        method: 'GET'
+    })
+      .then((res) => res.json())
+      .then(({ items }) => {
         dispatch(fetchStatusActions.markFetchDone());
         dispatch(fetchStatusActions.markFetchingFinished());
         dispatch(itemsActions.addInitialItems(items[0]));
-    });
-
-
-    return () => {
-        controller.abort();
-    };
-}, [fetchStatus]);
-return<>
-
-</>
-}
+      });
+  }, [fetchStatus]);
+  return <></>;
+};
 
 export default FetchItems;
